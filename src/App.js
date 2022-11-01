159a944638fc9767184ffde1403d8b3266b2e0cd
lamnaourm <mohammed.lamnaour@gmail.com>
2022-11-01 10:05:36 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Categories from "./components/Categories";
+import DetailProd from "./components/DetailProd";
 import Home from "./components/Home";
 import ProdsCategorie from "./components/ProdsCategorie";
 import Produits from "./components/Produits";
@@ -17,7 +18,10 @@ function App() {
               <Route index element={<Categories />} />
               <Route path=":catId" element={<ProdsCategorie />} />
           </Route>
-          <Route path="produits" element={<Produits />} />
+          <Route path="produits" >
+              <Route index element={<Produits />} />
+              <Route path=":prodId" element={<DetailProd />} />
+          </Route>
         </Route>
       </Routes>
     </div>
--- src/components/Produit.jsx
+++ src/components/Produit.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Produit = (props) => {
+
+    const navigate = useNavigate();
     return (
         <div className='card'>
              <img src={props.produit.images[0]}/>
             <h2>{props.produit.title}</h2>
-            <button>Plus de details</button>
+            <button onClick={() => navigate(`/produits/${props.produit.id}`)}>Plus de details</button>
         </div>
     );
 }
